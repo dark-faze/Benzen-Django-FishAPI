4db67222f3da6645cc47e41122a492d2d42aebd0
dark-faze <68679017+dark-faze@users.noreply.github.com>
2022-04-09 10:42:19 +0530

    added resizing feature using pillow

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Record
 from .serializers import RecordSerializer
+from PIL import Image
+ 
 
 @api_view(['GET'])
 def getData(request):
@@ -14,4 +16,13 @@ def postData(request):
     serializer = RecordSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        fishname = str(request.data[u'name'])
+        filename = str(request.data[u'image'].name)
+        # print(fishname + '/' + filename)
+        imgDir = fishname + '/' + filename
+        imgDir = imgDir.replace(' ','_')
+        img = Image.open("./media/images/" + imgDir)
+        resImg = img.resize((140,140))
+        resImg.save("./media/images/" + imgDir)
+
     return Response(serializer.data)
--- base/models.py
+++ base/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Record(models.Model):
+    def nameFile(instance, filename):
+      # print(str(instance.name))
+      return '/'.join(['images', str(instance.name), filename])
     name = models.CharField(max_length=20)
     species = models.CharField(max_length=20)
     weight = models.IntegerField()
     length = models.IntegerField()
-    # image = models.ImageField(upload_to="pictures",blank=True);
+    image = models.ImageField(upload_to=nameFile,blank=True);
     latitude = models.DecimalField(max_digits=10,decimal_places=3)
     longitude = models.DecimalField(max_digits=10,decimal_places=3)
     timestamp = models.DateTimeField(auto_now_add = True)
